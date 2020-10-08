161644880a6b85de9c87f691ee94b75c9bc42ac7
Virgile Bello <virgile@xenko.com>
2020-10-08 21:58:47 +0200

    test (not to push in final PR)

--- sources/tools/Stride.NuGetLoader/Program.cs
+++ sources/tools/Stride.NuGetLoader/Program.cs
@@ -14,11 +14,13 @@ namespace Stride.NuGetLoader
         static void Main(string[] args)
         {
             // Get loader data (text file, format is "PackageName/PackageId")
-            var loaderDataFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Stride.NuGetLoader.loaderdata");
-            var loaderData = File.ReadLines(loaderDataFile).First().Split('/');
+            //var loaderDataFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Stride.NuGetLoader.loaderdata");
+            //var loaderData = File.ReadLines(loaderDataFile).First().Split('/');
 
-            var packageName = loaderData[0];
-            var packageVersion = loaderData[1];
+            //var packageName = loaderData[0];
+            //var packageVersion = loaderData[1];
+            var packageName = "Stride.GameStudio";
+            var packageVersion = "4.0.0.1-beta04";
 
             NuGetAssemblyResolver.SetupNuGet(packageName, packageVersion);
             AppDomain.CurrentDomain.ExecuteAssemblyByName(packageName, args);
